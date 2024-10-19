4b3aae387bba08aae2bd4cb11c62e3351f7856a4
kaeppp <aiueoppp10@gmail.com>
2024-10-19 12:45:13 +0900

    tailwindcss文字の表示の大きさ変更

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -14,12 +14,12 @@
     <body>
         <div class="bg-yellowkou pt-10 px-14 pb-14">
             <div class="p-6 px-20 bg-yelloekou_2">
-                <h1 class="text-center sm:text-8xl font-dela mx-auto container pt-8 pl-6">防災用食品管理リスト</h1>
+                <h1 class="text-center xt-2xl md:text-4xl lg:text-8xl font-dela md:mx-auto container pt-8 pl-6">防災用食品管理リスト</h1>
                 {{--<a href="/items/create">新規追加</a>--}}
                 <form action="/items/create" method="post">
                     @csrf
-                    <div class="font-dela sm:text-4xl w-72 my-16 ml-7 mr-7 px-7 py-5 bg-brown25l rounded-full text-black hover:bg-brown cursor-pointer">
-                        <button type="button" onclick="location.href='/items/create'" class="border-dashed border-4 rounded-full px-10 py-8 border-white">新規追加</button>
+                    <div class="tracking-wider font-dela sm:text-4xl w-80 my-16 ml-7 mr-7 px-7 py-7 bg-brown25l rounded-full text-black hover:bg-brown cursor-pointer">
+                        <button type="button" onclick="location.href='/items/create'" class="border-dashed border-4 rounded-full px-10 py-4 border-white w-full">新規追加</button>
                     </div>
                 </form>
                 
@@ -28,25 +28,25 @@
                     @foreach($items as $item)
                         <div class="flex gap-4 justify-center mb-8">
                             <!--<div class=item>-->
-                                <div class="item block w-2/3 p-8 bg-white border-8 border-brown25l rounded-lg shadow-md justify-center">
+                                <div class="item block w-2/3 p-8 bg-white border-2 border-brown25l shadow-md justify-center">
                                     <h2 class="text-xl pb-3 font-bold">{{ $item->name }}</h2>
                                     <p class="pb-2">個数：{{ $item->stock }}個</p>
                                     <p class="expiration_date pb-2">賞味・消費期限：{{ $item->expiration_date }}</p>
-                                    <p class="pb-2">メモ：{{ $item->memo }}</p>
+                                    <p class="pb-2 truncate">メモ：{{ $item->memo }}</p>
                                     <p class="pt-6 pb-2 text-right text-gray-500">最終更新日：{{ $item->updated_at }}</p>
                                 </div>
                             <!--</div>-->
                             <div class="w-36 flex flex-col gap-8 rounded-full justify-center ">
                                 <form action="/items/{{ $item->id }}/edit" method="get">
                                     <div class="size-32 font-dela border border-blue1 bg-blue1 rounded-full flex justify-center items-center shadow-md hover:bg-blue2">
-                                        <button type="submit" class="text-2xl size-28 border-dashed border-4 rounded-full border-white">編集</button>
+                                        <button type="submit" class="tracking-wider text-2xl size-28 border-dashed border-4 rounded-full border-white">編集</button>
                                     </div>
                                 </form>
                                 <form action="items/{{ $item->id }}" id="form_{{ $item->id }}" method="post">
                                     @csrf
                                     @method('DELETE')
                                     <div class="size-32 font-dela border border-blue1 bg-blue1 rounded-full flex justify-center items-center shadow-md p-2 hover:bg-blue2">
-                                        <button type="button" onclick="deleteItem({{ $item->id }}) " class="text-2xl size-28 border-dashed border-4 rounded-full border-white">削除</button>
+                                        <button type="button" onclick="deleteItem({{ $item->id }}) " class="tracking-wider text-2xl size-28 border-dashed border-4 rounded-full border-white">削除</button>
                                     </div>
                                 </form>
                             </div>
